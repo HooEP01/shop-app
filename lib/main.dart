e83016586610f20ef6a1e0fe97d21bb11e76b041
HOOEP01 <B200152B@sc.edu.my>
2023-03-24 10:10:27 +0800

    add products provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
 import './screens/products_overview_screen.dart';
 import './screens/product_detail_screen.dart';
+import './providers/products.dart';
 
 void main() => runApp(const MyApp());
 
@@ -9,7 +12,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return ChangeNotifierProvider(
+      create: (ctx) => Products(),
+      child: MaterialApp(
         title: 'MyShop',
         theme: ThemeData(
           colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.purple)
@@ -18,7 +23,9 @@ class MyApp extends StatelessWidget {
         ),
         home: ProductsOverviewScreen(),
         routes: {
-          ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
-        });
+          ProductDetailScreen.routeName: (ctx) => const ProductDetailScreen(),
+        },
+      ),
+    );
   }
 }
--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -13,32 +13,36 @@ class ProductItem extends StatelessWidget {
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
-          footer: GridTileBar(
-            backgroundColor: Colors.black54,
-            leading: IconButton(
-              icon: const Icon(Icons.favorite),
-              color: Theme.of(context).colorScheme.secondary,
-              onPressed: () {},
-            ),
-            title: Text(
-              title,
-              textAlign: TextAlign.center,
-            ),
-            trailing: IconButton(
-              icon: const Icon(Icons.shopping_cart),
-              color: Theme.of(context).colorScheme.secondary,
-              onPressed: () {},
-            ),
+        footer: GridTileBar(
+          backgroundColor: Colors.black54,
+          leading: IconButton(
+            icon: const Icon(Icons.favorite),
+            color: Theme.of(context).colorScheme.secondary,
+            onPressed: () {},
           ),
-          child: GestureDetector(
-            onTap: () {
-              Navigator.of(context).pushNamed(ProductDetailScreen.routeName, arguments: id,);
-            },
-            child: Image.network(
-              imageUrl,
-              fit: BoxFit.cover,
-            ),
-          )),
+          title: Text(
+            title,
+            textAlign: TextAlign.center,
+          ),
+          trailing: IconButton(
+            icon: const Icon(Icons.shopping_cart),
+            color: Theme.of(context).colorScheme.secondary,
+            onPressed: () {},
+          ),
+        ),
+        child: GestureDetector(
+          onTap: () {
+            Navigator.of(context).pushNamed(
+              ProductDetailScreen.routeName,
+              arguments: id,
+            );
+          },
+          child: Image.network(
+            imageUrl,
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
     );
   }
 }
