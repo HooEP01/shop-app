bc954f1c442580d6e4ff1e81cf1c5a93f119a810
HOOEP01 <B200152B@sc.edu.my>
2023-03-26 09:33:49 +0800

    add cart dismissible, remove cart item

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -55,10 +55,11 @@ class CartScreen extends StatelessWidget {
             child: ListView.builder(
               itemCount: cart.itemCount,
               itemBuilder: (ctx, i) => CartItem(
-                cart.items.values.toList()[i]!.id,
-                cart.items.values.toList()[i]!.price,
-                cart.items.values.toList()[i]!.quantity,
-                cart.items.values.toList()[i]!.title,
+                cart.items.values.toList()[i].id,
+                cart.items.keys.toList()[i],
+                cart.items.values.toList()[i].price,
+                cart.items.values.toList()[i].quantity,
+                cart.items.values.toList()[i].title,
               ),
             ),
           ),
