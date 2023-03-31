1a36243c039b817180564b08733e73d815564e35
HOOEP01 <B200152B@sc.edu.my>
2023-03-31 13:41:16 +0800

    error handle of product add function

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -105,7 +105,23 @@ class _EditProductScreenState extends State<EditProductScreen> {
     } else {
       Provider.of<Products>(context, listen: false)
           .addProduct(_editedProduct)
-          .then((_) {
+          .catchError((error) {
+        return showDialog<Null>(
+          context: context,
+          builder: (ctx) => AlertDialog(
+            title: const Text('An error occured!'),
+            content: const Text('Something went wrong!'), // error.toString()
+            actions: [
+              TextButton(
+                child: const Text('Okay!'),
+                onPressed: () {
+                  Navigator.of(ctx).pop();
+                },
+              ),
+            ],
+          ),
+        );
+      }).then((_) {
         setState(() {
           _isLoading = false;
         });
